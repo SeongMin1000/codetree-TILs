dec857b8e3b9c63b6ee089d7745eceba61eed7e8
SeongMin1000 <83529503+SeongMin1000@users.noreply.github.com>
2024-08-07 20:57:00 +0900

    Title: 만나는 그 순간, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240807/만나는 그 순간/the-moment-we-meet.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+int lineA[1000]={};
+int lineB[1000]={};
+int total=1;
+
+int main() {
+    int n,m;
+    cin>>n>>m;
+    for(int i=0; i<n; i++){
+        string d;
+        int t;
+        cin>>d>>t;
+
+        for(int j=total; j<total+t; j++){
+           if(d=="L"){
+            lineA[j]=lineA[j-1]-1;
+           }
+           else{
+            lineA[j]=lineA[j-1]+1;
+           }
+        }
+        total+=t;
+    }
+
+    total=1;
+    for(int i=0; i<m; i++){
+        string d;
+        int t;
+        cin>>d>>t;
+
+        for(int j=total; j<total+t; j++){
+           if(d=="L"){
+            lineB[j]=lineB[j-1]-1;
+           }
+           else{
+            lineB[j]=lineB[j-1]+1;
+           }
+        }
+        total+=t;
+
+    }
+
+    for(int i=1; i<1000; i++){
+        if(lineA[i]==lineB[i]){
+            cout<<i;
+            break;
+        }
+    }
+    return 0;
+}
