9eb7c6d74349fc9de612db697db9398f5438d983
SeongMin1000 <83529503+SeongMin1000@users.noreply.github.com>
2024-08-07 21:38:45 +0900

    Title: 선두를 지켜라, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240807/선두를 지켜라/keep-the-lead.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int whereA[1000001];
+int whereB[1000001];
+int total=1;
+
+int main() {
+    int n,m;
+    cin>>n>>m;
+    for(int i=0; i<n; i++){
+        int v,t;
+        cin>>v>>t;
+
+        for(int j=total; j<total+t; j++){
+            whereA[j]=whereA[j-1]+v;
+        }
+        total+=t;
+    }
+
+    total=1;
+    for(int i=0; i<n; i++){
+        int v,t;
+        cin>>v>>t;
+
+        for(int j=total; j<total+t; j++){
+            whereB[j]=whereB[j-1]+v;
+        }
+        total+=t;
+    }
+
+    int count=0;
+    for(int i=1; i<=total; i++){
+        if(whereA[i]!=whereB[i]){
+            if(whereA[i-1]==whereB[i-1] && i-1!=0){
+                count++;
+            }
+        }
+    }
+    cout<<count;
+    return 0;
+}
