4d2ac4879c425a036e8cf33621a5adbc52831d79
SeongMin1000 <83529503+SeongMin1000@users.noreply.github.com>
2024-08-17 20:35:23 +0900

    Title: 거울에 레이저 쏘기 2, Time: 1008ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 240817/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+char arr[1001][1001];
+int dx[4]={1,0,-1,0};
+int dy[4]={0,1,0,-1};
+int dir;
+
+int main() {
+    int n,k;
+    cin>>n;
+    for(int i=0; i<n; i++){
+        string mirror;
+        cin>>mirror;
+
+        for(int j=0; j<mirror.length(); j++){
+            arr[i][j]=mirror[j];
+        }
+    }
+    cin>>k;
+
+    int startR=0;
+    int startC=0;
+    if((k-1)/n==0){
+        dir=1;
+        startR=0;
+        startC=k-1;
+    } 
+    else if((k-1)/n==1){
+        dir=2;
+        startR=(k-1)%n;
+        startC=n-1;
+    } 
+    else if((k-1)/n==2){
+        dir=3;
+        startR=n-1;
+        startC=(n-1)-(k-1)%n;
+    } 
+    else{
+        dir=0;
+        startR=(n-1)-(k-1)%n;
+        startC=0;
+    } 
+
+    bool inging=true;
+    int count=1;
+    if(arr[startR][startC]=='/'){
+        dir=3-dir;
+    }
+    else{
+        dir=(5-dir)%4;
+    }
+
+    while(inging){
+        for(int i=0; i<4; i++){
+            int nx=startC+dx[dir];
+            int ny=startR+dy[dir];
+
+            if(0<=nx && nx<n && 0<=ny && ny<n){
+                if(arr[startR][startC]=='/'){
+                    dir=3-dir;
+                }
+                else{
+                    dir=(5-dir)%4;
+                }
+                count++;
+            }
+            else{
+                cout<<count;
+                inging=false;
+                break;
+            }
+            startC=nx;
+            startR=ny;
+        }
+    }
+
+    return 0;
+}
