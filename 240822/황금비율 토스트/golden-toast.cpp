90559efb711a275a850f52c72e4338f82ee2642e
SeongMin1000 <83529503+SeongMin1000@users.noreply.github.com>
2024-08-22 19:21:40 +0900

    Title: 황금비율 토스트, Time: 16ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240822/황금비율 토스트/golden-toast.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+list<char> breads;
+
+int main() {
+    int n,m;
+    string bread;
+    cin>>n>>m>>bread;
+
+    for(int i=0; i<n; i++){
+        breads.push_back(bread[i]);
+    }
+    auto cur=breads.end();
+
+    for(int i=0; i<m; i++){
+        string dir;
+        cin>>dir;
+        if(dir=="L"){
+            if(cur!=breads.begin())
+                cur--;
+        }
+        else if(dir=="R"){
+            if(cur!=breads.end())
+                cur++;
+        }
+        else if(dir=="D"){
+            if(cur!=breads.end())
+                breads.erase(cur);
+        }
+        else{
+            char w;
+            cin>>w;
+            breads.insert(cur,w);
+        }
+    }
+
+    for(auto it=breads.begin(); it!=breads.end(); it++){
+        cout<<*it;
+    }
+    return 0;
+}
